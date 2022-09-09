e98958e9ba0abc2f86846fa20bba43700df90523
Doruk <ozan@roof.games>
2022-09-09 17:20:36 +0300

    a

--- Assets/Scripts/Compass.cs
+++ Assets/Scripts/Compass.cs
@@ -9,6 +9,6 @@ public class Compass : MonoBehaviour
     [SerializeField] private GameObject _example;
     void Update()
     {
-        transform.DOLookAt(_example.transform.position, 1, AxisConstraint.None,Vector3.up);
+        transform.DOLookAt(_example.transform.position, 1,AxisConstraint.Z,Vector3.up);
     }
 }
